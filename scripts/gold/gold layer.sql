2d9a0ae7664c5943c72d23ca51e6283bcc43e8cd
Otilia Macau <otilia.cecilia.lungu@gmail.com>
2026-02-05 18:14:32 +0200

    Update gold layer.sql

--- scripts/gold/gold layer.sql
+++ scripts/gold/gold layer.sql
@@ -1,3 +1,17 @@
+/*
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
 SELECT
 	ci.cst_id,
 	ci.cst_key,
@@ -51,7 +65,13 @@ FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
 ON ci.cst_key = ca.cid;
  
-
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+    DROP VIEW gold.dim_customers;
+GO
+	
 CREATE VIEW gold.dim_customers AS
 SELECT
 	ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
@@ -72,7 +92,12 @@ ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
 ON ci.cst_key = la.cid;
 
-SELECT * FROM gold.dim_customers;
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+IF OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
+    DROP VIEW gold.dim_products;
+GO
 
 CREATE VIEW gold.dim_products AS
 SELECT
@@ -92,7 +117,12 @@ LEFT JOIN silver.erp_px_cat_g1v2 pc
 ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL  -- filter out all historical data
 
-SELECT * FROM gold.dim_products;
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
 
 CREATE VIEW gold.fact_sales AS
 SELECT
@@ -110,5 +140,3 @@ LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
 ON sd.sls_cust_id = cu.customer_id;
-
-SELECT * FROM gold.fact_sales;
